647940072cc38736cf6f02626984bbc092022a38
wlgn8648 <67787453+wlgn8648@users.noreply.github.com>
2020-07-12 19:36:45 +0900

    Update Block.h

--- Block.h
+++ Block.h
@@ -1,6 +1,3 @@
-//Honor code
-//나는 이 프로그래밍 과제를 다른 사람의 부적절한 도움 없이 완수하였습니다.
-//I completed this programming task without the improper help of others.
 #ifndef POINT_H
 #define POINT_H
 #include <string>
